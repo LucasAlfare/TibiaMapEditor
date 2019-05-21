d126d9b1d918cbb3ef77e67cee9cdf196c7c90aa
LucasAlfare <god-lucas@hotmail.com>
2019-05-21 12:53:54 -0300

    Inicial

---
+++ src/com/Gui.java
@@ -0,0 +1,86 @@
+package com;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Gui extends JFrame {
+
+    private JRelativeLayout rootLayout;
+
+    private JPanel pA, pB;
+
+    private JRelativeLayout aLayout;
+    private JComboBox<String> categories;
+    private JScrollPane itemsScrollPane;
+    private JList<String> items;
+
+    private JRelativeLayout bLayout;
+    private JScrollPane canvasScrollPane;
+    private JMapEditorCanvas canvas;
+
+    public Gui(){
+        setSize(800, 800);
+        setExtendedState(Frame.MAXIMIZED_BOTH);
+        setDefaultCloseOperation(3);
+        setLocationRelativeTo(null);
+
+        pA = new JPanel();
+        pA.setName("pA");
+
+        pB = new JPanel();
+        pB.setName("pB");
+
+        categories = new JComboBox<>();
+        categories.setName("categories");
+        setupCategories(new String[]{"GROUND", "TOWN", "NATURE", "INTERIOR"}, categories);
+
+        itemsScrollPane = new JScrollPane();
+        items = new JList<>();
+        itemsScrollPane.setName("itemsPane");
+        setupItems(new String[]{"A", "B", "C", "D", "E", "F"}, items);
+
+        canvas = new JMapEditorCanvas(50, 50);
+        canvas.setName("canvas");
+        canvasScrollPane = new JScrollPane(canvas);
+        canvasScrollPane.setName("canvasScrollPane");
+
+        rootLayout = new JRelativeLayout();
+        aLayout = new JRelativeLayout();
+        bLayout = new JRelativeLayout();
+
+        pA.setLayout(aLayout);
+        pA.add(categories,
+                "parentTop=true parentStart=true width=matchParent height=30");
+        pA.add(itemsScrollPane,
+                "bellow=categories width=matchParent height=matchParent");
+
+        pB.setLayout(bLayout);
+        pB.add(canvasScrollPane,
+                "parentTop=true parentStart=true width=matchParent height=matchParent");
+
+        setLayout(rootLayout);
+        add(pA,
+                "parentTop=true parentStart=true width=10% height=matchParent");
+        add(pB,
+                "parentTop=true endOf=pA width=matchParent height=matchParent");
+    }
+
+    private void setupCategories(String[] content, JComboBox<String> target) {
+        target.setModel(new DefaultComboBoxModel<>(content));
+    }
+
+    private void setupItems(String[] content, JList<String> target) {
+        target.setModel(new AbstractListModel<String>() {
+            @Override
+            public int getSize() {
+                return content.length;
+            }
+
+            @Override
+            public String getElementAt(int i) {
+                return content[i];
+            }
+        });
+        itemsScrollPane.setViewportView(target);
+    }
+}
---
+++ src/com/JMapEditorCanvas.java
@@ -0,0 +1,165 @@
+package com;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class JMapEditorCanvas extends JComponent implements Scrollable {
+
+    private int w, h;
+    private int[][] tiles;
+
+    private int currX, currY;
+    public int currentSelectedID = 253; //TODO: permitir usuário alterar esse valor
+
+    private Dimension preferredScrollableViewportSize;
+
+    public JMapEditorCanvas(int w, int h) {
+        this.w = w;
+        this.h = h;
+
+        tiles = new int[w][h];
+        for (int[] tile : tiles) {
+            Arrays.fill(tile, -1);
+        }
+
+        this.setPreferredScrollableViewportSize(new Dimension((w+1) * 32, (h+1) * 32));
+
+        addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent mouseEvent) {
+                super.mouseDragged(mouseEvent);
+                Point p = mouseEvent.getPoint();
+                int i = p.x/32; int j = p.y/32;
+                if ((i >= 0 && j >= 0) && (i < JMapEditorCanvas.this.w && j < JMapEditorCanvas.this.h)){
+                    tiles[p.x/32][p.y/32] = currentSelectedID;
+                    repaint();
+                }
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent mouseEvent) {
+                super.mouseMoved(mouseEvent);
+                Point p = mouseEvent.getPoint();
+                int i = p.x/32; int j = p.y/32;
+                if ((i >= 0 && j >= 0) && (i < JMapEditorCanvas.this.w && j < JMapEditorCanvas.this.h)){
+                    currX = p.x;
+                    currY = p.y;
+                    repaint();
+                }
+            }
+        });
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent mouseEvent) {
+                super.mousePressed(mouseEvent);
+                Point p = mouseEvent.getPoint();
+                int i = p.x/32; int j = p.y/32;
+                if ((i >= 0 && j >= 0) && (i < JMapEditorCanvas.this.w && j < JMapEditorCanvas.this.h)){
+                    tiles[p.x/32][p.y/32] = currentSelectedID;
+                    repaint();
+                }
+            }
+        });
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(w * 32, h * 32);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D gg = (Graphics2D) g;
+        try {
+            atualizar(gg);
+            mouseCursor(gg);
+            gg.dispose();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void atualizar(Graphics2D g) throws IOException {
+        for (int i = 0; i < tiles.length * 32; i += 32) {
+            for (int j = 0; j < tiles[i/32].length * 32; j += 32) {
+                g.setColor(Color.DARK_GRAY);
+                if (tiles[i/32][j/32] != -1){
+                    g.drawImage(
+                            ImageIO.read(new File("src/com/sprites/10304.gif")),
+                            i,
+                            j,
+                            this
+                    );
+                } else {
+                    g.fillRect(i, j, 32, 32);
+                }
+                g.setColor(Color.BLACK);
+                g.drawRect(i, j, 32, 32);
+            }
+        }
+    }
+
+    private void mouseCursor(Graphics2D g) throws IOException {
+        g.drawImage(
+                imgW_ModedAlpha(ImageIO.read(new File("src/com/sprites/10304.gif")), 180),
+                (currX / 32) * 32,
+                (currY / 32) * 32,
+                this);
+    }
+
+    private static BufferedImage imgW_ModedAlpha(BufferedImage modMe, double modAmount) {
+        for (int x = 0; x < modMe.getWidth(); x++) {
+            for (int y = 0; y < modMe.getHeight(); y++) {
+                //
+                int argb = modMe.getRGB(x, y); //always returns TYPE_INT_ARGB
+                int alpha = (argb >> 24) & 0xff;  //isolate alpha
+
+                alpha *= modAmount; //similar distortion to tape saturation (has scrunching effect, eliminates clipping)
+                alpha &= 0xff;      //keeps alpha in 0-255 range
+
+                argb &= 0x00ffffff; //remove old alpha info
+                argb |= (alpha << 24);  //add new alpha info
+                modMe.setRGB(x, y, argb);
+            }
+        }
+
+        return modMe;
+    }
+
+    @Override
+    public Dimension getPreferredScrollableViewportSize() {
+        return preferredScrollableViewportSize;
+    }
+
+    public void setPreferredScrollableViewportSize(Dimension size) {
+        this.preferredScrollableViewportSize = size;
+    }
+
+    @Override
+    public int getScrollableUnitIncrement(Rectangle rectangle, int i, int i1) {
+        return 32;
+    }
+
+    @Override
+    public int getScrollableBlockIncrement(Rectangle rectangle, int i, int i1) {
+        return 32;
+    }
+
+    @Override
+    public boolean getScrollableTracksViewportWidth() {
+        return false;
+    }
+
+    @Override
+    public boolean getScrollableTracksViewportHeight() {
+        return false;
+    }
+}
