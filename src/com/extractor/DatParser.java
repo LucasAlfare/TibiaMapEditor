13eb250e8bf17de394ecf87e5674a5dac005a093
LucasAlfare <god-lucas@hotmail.com>
2019-05-24 11:12:52 -0300

    algumas organicoes e inicio do DatParser

--- src/com/extractor/DatParser.java
+++ src/com/extractor/DatParser.java
@@ -1,4 +1,17 @@
 package com.extractor;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class DatParser {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("xyz.dat");
+        Scanner scnr = new Scanner(file);
+        while(scnr.hasNextLine()){
+            String line = scnr.nextLine();
+            System.out.println(line);
+        }
+    }
 }
