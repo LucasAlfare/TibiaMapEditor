fac7cbb71de43169270fecf902dd31b532d71e9a
Lucas Sousa <god-lucas@hotmail.com>
2019-07-13 13:05:36 -0300

    estrutura geral modificada

---
+++ src/com/Main.java
@@ -0,0 +1,24 @@
+package com;
+
+import misc.C;
+
+import javax.swing.*;
+
+public class Main extends JFrame {
+
+    public Main() {
+        int size = 10;
+        setSize((size * C.TS) + 1, (size * C.TS) + 1);
+        setDefaultCloseOperation(3);
+        setLocationRelativeTo(null);
+
+        MCanvas mCanvas = new MCanvas();
+        add(mCanvas);
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
